9f5fd75d5c30649eb2ce02746ebf310df86967b1
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-05-29 14:41:22 +0900

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Tadashi Yoshimura on 2020/05/29.
+//  Copyright © 2020 Tadashi Yoshimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
