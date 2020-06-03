c513c6720754e9c58a1fbc0fcc317ca7beaef391
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-06-03 14:38:45 +0900

    アニメーション

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var basicCard: UIView!
+    
+    var centerOfCard:CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        centerOfCard = basicCard.center
     }
 
-
+    
+    @IBAction func swipeCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        
+        card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+        
+        if sender.state == UIGestureRecognizer.State.ended{
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.centerOfCard
+            })
+           
+        }
+    }
+    
 }
 
