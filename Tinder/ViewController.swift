0c5fe6a7af4c9a0f0003703bd4933018a10883ed
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-06-04 11:03:59 +0900

    右・左スワイプ

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -27,6 +27,23 @@ class ViewController: UIViewController {
         card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
         
         if sender.state == UIGestureRecognizer.State.ended{
+            //左にスワイプ
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    card.center = CGPoint(x: card.center.x - 250, y: card.center.y)
+                })
+                return  //この後の元に戻る処理をしたくない為、関数から抜ける
+            //右にスワイプ
+            }else if card.center.x > self.view.frame.width - 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    card.center = CGPoint(x: card.center.x + 250, y: card.center.y)
+                })
+                return
+            }
+            
+            
+            
+            //元に戻る処理
             UIView.animate(withDuration: 0.2, animations: {
                 card.center = self.centerOfCard
             })
